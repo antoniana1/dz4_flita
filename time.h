36fb108253d3180d7231b47e6b7c7565c37754e2
antoniana1 <114882391+antoniana1@users.noreply.github.com>
2023-05-26 23:19:17 +0300

    Add files via upload

---
+++ time.h
@@ -0,0 +1,16 @@
+#ifndef FLITATEST_TIME_H
+#define FLITATEST_TIME_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define START_DATA_SIZE 100
+#define END_DATA_SIZE 500
+#define RANDOM_DATA_SIZE_STEP 10
+#define DENOMINATOR 30
+
+void GiveRandFile1(int numberOfNumbers); // Запись рандомных значений в файл для считывания
+
+#endif //FLITATEST_TIME_H
